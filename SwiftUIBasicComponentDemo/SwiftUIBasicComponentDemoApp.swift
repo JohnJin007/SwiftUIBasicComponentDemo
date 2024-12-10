a55b24e4877780d9fd1decb838db1cff62a7b982
JohnJin007 <2219566118@qq.com>
2024-12-10 17:01:43 +0800

    Initial Commit

---
+++ SwiftUIBasicComponentDemo/SwiftUIBasicComponentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicComponentDemoApp.swift
+//  SwiftUIBasicComponentDemo
+//
+//  Created by v_jinlilili on 2024/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicComponentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
